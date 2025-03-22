6878d8b73ace3f6d9d0a3bf9dcf9a9befe0a9425
syeduroojkamal <syeduroojkamal26@gmail.com>
2025-03-22 08:57:25 +0530

    more feat

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ export default function App() {
     <>
       <p className="text-center">Git</p>
       <p className="text-red-400">Feature</p>
+      <p className="text-yellow-300">more Feature</p>
     </>
   );
 }
